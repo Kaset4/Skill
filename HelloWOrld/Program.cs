f30500363e0a6b705c6d1cde2168da4eac53ea3a
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-01-18 17:49:11 +0400

    Add files via upload

---
+++ HelloWOrld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace HelloWOrld;
+
+class Program
+{
+    static  void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        string text = File.ReadAllText("C:\\Users\\Егор\\Desktop\\1.txt");
+
+        char[] sym = new char[] {' ', '\r', '\n' };
+          
+        string[] words = text.Split(sym, StringSplitOptions.RemoveEmptyEntries);
+
+        List<string> ListTest = new List<string>();
+        LinkedList<string> LinkedListTest = new LinkedList<string>();
+
+        stopwatch.Start();
+        for (int i = 0; i < words.Length; i++)
+        {
+            ListTest.Add(words[i]);
+        }
+        stopwatch.Stop();
+
+        System.Console.WriteLine($"ListTest:  {stopwatch.ElapsedTicks}");
+
+        stopwatch.Reset();
+
+        stopwatch.Start();
+        for (int i = 0; i < words.Length; i++)
+        {
+            LinkedListTest.AddLast(words[i]);
+        }
+        stopwatch.Stop();
+
+        System.Console.WriteLine($"LinkedListTest:  {stopwatch.ElapsedTicks}");
+    }
+}
