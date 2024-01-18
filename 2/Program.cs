f59633a24061fd9f122efaf5e794a26997d5c369
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-01-18 18:13:30 +0400

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace HelloWOrld;
+
+class Program
+{
+    static  void Main(string[] args)
+    {
+        string text = File.ReadAllText("C:\\Users\\Егор\\Desktop\\1.txt");
+
+        char[] sym = new char[] {' ', '\r', '\n' };
+          
+        string[] words = text.Split(sym, StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, int> wordsDict = new Dictionary<string, int>();
+
+        foreach (var item in words)
+        {
+            if(wordsDict.ContainsKey(item)) 
+            {
+                wordsDict[item]++;
+            } else
+            {
+                wordsDict[item] = 1;
+            }
+        }
+        
+        var topWords = wordsDict.OrderByDescending(c => c.Value).Take(10);
+
+        foreach (var item in topWords)
+        {
+            System.Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
