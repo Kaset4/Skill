0276d7b3f06d37f1da595513f02878a39c45482a
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-01-20 22:09:28 +0400

    Add files via upload

---
+++ 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace HelloWOrld;
+
+class Program
+{
+   static string [] GetAllStudents( Classroom [] classes )
+       {
+           return  (from classroom in classes from student in classroom.Students select student).ToArray();
+       }
+      
+       public class Classroom
+       {
+           public List<string> Students = new List<string>();
+       }
+    static  void Main(string[] args)
+    {
+        var classes = new []
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+           var allStudents = GetAllStudents(classes);
+         
+           Console.WriteLine(string.Join(" ", allStudents));
+    }
+}
