39e779e0bfe55bea1e43f5464b9ab1179bbb22a5
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-01-30 18:48:06 +0400

    Add files via upload

---
+++ SocialNetwork/SocialNetwork/BLL/Services/FriendService.cs
@@ -0,0 +1,60 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class FriendService
+    {
+        IUserRepository userRepository;
+        IFriendRepository friendRepository;
+
+        public FriendService()
+        {
+            userRepository = new UserRepository();
+            friendRepository = new FriendRepository();
+        }
+
+        public void AddFriend(FriendData friendData)
+        {
+            if (String.IsNullOrEmpty(friendData.Email))
+                throw new ArgumentNullException();
+
+            var findUserEntity = this.userRepository.FindByEmail(friendData.Email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            if (CheckForDublicateFriendsEntry(friendData, findUserEntity))
+                throw new UserNotFoundException();
+
+            var friendEntity = new FriendEntity()
+            {
+                user_id = friendData.User_id,
+                friend_id = findUserEntity.id
+            };
+
+            if (this.friendRepository.Create(friendEntity) == 0)
+                throw new Exception();
+        }
+
+        private bool CheckForDublicateFriendsEntry(FriendData friendData, UserEntity user)
+        {
+            IEnumerable<FriendEntity> friendEntities = friendRepository.FindAllByUserId(friendData.User_id);
+
+            foreach (var friendEntity in friendEntities)
+            {
+                if (friendEntity.friend_id == user.id)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SocialNetwork/SocialNetwork/PLL/Views/UserFiendView.cs
@@ -0,0 +1,54 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserFiendView
+    {
+        FriendService friendService;
+
+        public UserFiendView(FriendService friendService)
+        {
+            this.friendService = friendService;
+        }
+
+        public void Show(User user)
+        {
+            var friendData = new FriendData();
+
+            Console.WriteLine("Добавить в друзья: ");
+            Console.WriteLine("Введите почтовый адрес друга:");
+
+            friendData.Email = Console.ReadLine();
+            friendData.User_id = user.Id;
+
+            try
+            {
+                friendService.AddFriend(friendData);
+
+                SuccessMessage.Show($"Вы успешно добавили в друзья: {friendData.Email}");
+            }
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователь не найден!");
+            }
+            catch (ArgumentNullException)
+            {
+                AlertMessage.Show("Введите корректное значение!");
+            }
+
+            catch (Exception)
+            {
+                AlertMessage.Show("Произошла ошибка при отправке сообщения!");
+            }
+
+        }
+    }
+}
