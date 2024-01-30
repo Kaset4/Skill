4ffa35d65b82cbcd596005d5fe4b215d64f36dc5
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-01-30 19:05:57 +0400

    Add files via upload

---
+++ SocialNetwork/SocialNetwork/BLL/Models/FriendData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Models
+{
+    public class FriendData
+    {
+        public string Email { get; set; }
+        public int User_id { get; set; }
+        
+    }
+}
